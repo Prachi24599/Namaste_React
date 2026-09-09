6d6438a0a6b02809861bc1670a587bed013af192
Prachi24599 <prachi24599@gmail.com>
2026-09-09 23:00:43 +0200

    Example of Closure

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-// function Counter(){
-//     var counter = 0;
-//     return function incrementCounter(){
-//         counter++;
-//         console.log(counter);
-//     }
-// }
+//What is callback function in javascript?
 
-// var counter1 = Counter();
-// counter1();
-// counter1();
-// counter1();
+setTimeout(function(){
+    console.log("Timer");
+}, 5000);   
 
-// function x(){
-//     var a = 7;
-//     return function y(){
-//         console.log(a);
-//     }
-// }
-// var z = x();
-// //=====
-// z();
 
-function pranit(){
-    for(var i =1; i <=5; i++){
-        function print(i){
-            setTimeout(function(){
-                console.log(i)
-            }, i * 1000)
-        }
-        print(i);
-    }
-    console.log("Dudu")
+function x(y){
+    console.log("x called");
+    y();
 }
-pranit()
+
+x(function y(){
+    console.log("y called");
+})
