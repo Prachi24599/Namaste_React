7116ab2a9e5030595adfbc7150c6c278626e1c4c
Prachi24599 <prachi24599@gmail.com>
2026-09-09 23:12:31 +0200

    Closure with Click event listener

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 //What is callback function in javascript?
 
-setTimeout(function(){
-    console.log("Timer");
-}, 5000);   
+// setTimeout(function(){
+//     console.log("Timer");
+// }, 5000);   
 
 
-function x(y){
-    console.log("x called");
-    y();
+// function x(y){
+//     console.log("x called");
+//     y();
+// }
+
+// x(function y(){
+//     console.log("y called");
+// })
+
+function attachEventListener(){
+    var count = 0;
+    document.getElementById("clickMe").addEventListener("click", function xyz(){
+        console.log("Button Clicked!", count++)
+    })
 }
-
-x(function y(){
-    console.log("y called");
-})
+attachEventListener();
