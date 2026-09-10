13061f8bab256816d6cb140a24d52963a52f16b7
Prachi24599 <prachi24599@gmail.com>
2026-09-10 15:35:00 +0200

    Ex- setTimeout trust issues

--- index.js
+++ index.js
@@ -14,10 +14,28 @@
 //     console.log("y called");
 // })
 
-function attachEventListener(){
-    var count = 0;
-    document.getElementById("clickMe").addEventListener("click", function xyz(){
-        console.log("Button Clicked!", count++)
-    })
+// function attachEventListener(){
+//     var count = 0;
+//     document.getElementById("clickMe").addEventListener("click", function xyz(){
+//         console.log("Button Clicked!", count++)
+//     })
+// }
+// attachEventListener();
+
+//We can not trust setTimeout that It will execute code after specified amount of time
+console.log("start");
+setTimeout(function cb(){
+    console.log("Timer")
+}, 5000);
+console.log("End");
+
+
+//This loop will run for 10 sec
+//It will block main thread for 10 second
+let startDate = new Date().getTime();
+let endDate = startDate;
+while(endDate < startDate + 10000){
+    endDate = new Date().getTime();
 }
-attachEventListener();
+
+console.log("While Expires");
