f48d24e7b4162143f69e36e8bd32b904940606d8
Prachi24599 <prachi24599@gmail.com>
2026-09-11 23:43:37 +0200

    Map function - Higher order function

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-const radius = [2, 4, 6, 8];
+//map function 
+//map function is used to transform an array by applying a function to each of its elements and returning a new array with the transformed values.
+const numbers = [1, 2, 3, 4, 5, 6];
 
-//Find Area of Circle
-function area(radius){
-    return Math.PI * radius * radius;
+function double(number){
+    return number * 2;
 }
 
-function circumference(radius){
-    return 2 * Math.PI * radius;
-}
-
-function diameter(radius){
-    return 2 * radius;
-}
-
-//this points to the array on which the method is called
-Array.prototype.calculate = function(logic){
-    const output = [];
-    for(let i = 0; i < this.length; i++){
-        output.push(logic(this[i]));
-    }
-    return output;
-}
-
-console.log(radius.calculate(area))
-console.log(radius.calculate(circumference))
-console.log(radius.calculate(diameter))
+console.log(numbers.map(double))
+console.log(numbers.map(function(number){
+    return number * 2;
+}))
+console.log(numbers.map((number) => number * 2))
