ea3693edc33fb1bb87cbf05a7faf3b1e673db808
Prachi24599 <prachi24599@gmail.com>
2026-09-11 23:58:15 +0200

    reduce - higher order function

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-//map function 
-//map function is used to transform an array by applying a function to each of its elements and returning a new array with the transformed values.
-const numbers = [1, 2, 3, 4, 5, 6];
+//filter function
+//filter function is used to filter the values of the array based on the condition provided in the callback function. 
+//It returns a new array with all the elements that pass the test implemented by the provided function.
 
-function double(number){
-    return number * 2;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function isEven(num){
+    return num % 2 === 0;
 }
 
-console.log(numbers.map(double))
-console.log(numbers.map(function(number){
-    return number * 2;
-}))
-console.log(numbers.map((number) => number * 2))
+function isOdd(num){
+    return num % 2;
+}
+
+console.log(arr.filter(isEven))
+console.log(arr.filter(isOdd))
+
+console.log(arr.filter((num) => num > 5))
