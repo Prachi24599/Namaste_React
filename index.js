30b43c0a95010a6e9a6d2021283c396ad9821ad4
Prachi24599 <prachi24599@gmail.com>
2026-09-12 01:33:50 +0200

    Tricky Important Interview ques

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const data = [
     { firstname : "Bob", lastname: "Johnson", age: 30 },
     { firstname : "Alice", lastname: "Williams", age: 28 }
 ]
-//get full names of all people in the data array
-function getFullNames(data){
-    return data.map((person) => person.firstname + " " + person.lastname)
-}   
-console.log(getFullNames(data));
 
-// { 30 : 1, 25: 1, 28 : 1 }
-const countByAge = data.reduce((acc, curr) => {
-   if(acc[curr.age]){
-    acc[curr.age] =  ++acc[curr.age]
-   }else{
-    acc[curr.age] = 1;
-   }
-   return acc;
-}, {});
+//Find the first name of all the people whose age is less than 30
+const ageLessThanThirty = data.filter(person => person.age < 30).map(person => person.firstname)
 
-console.log(countByAge);
+console.log(ageLessThanThirty)
+
+const ageLessThanThirtyReduce = data.reduce((acc, curr) => {
+    if(curr.age < 30){
+        acc.push(curr.firstname);
+    }
+    return acc;
+}, []) 
+console.log(ageLessThanThirtyReduce)
