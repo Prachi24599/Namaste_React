75642d9156347657f4ec0e366631315fa8c43c82
Prachi24599 <prachi24599@gmail.com>
2026-09-13 01:31:45 +0200

    creating promise

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const github_api = "https://api.github.com/users/prachi24599"
+const cart = ["A", "B", "C"];
 
-const res = fetch(github_api);
-console.log(res);
-
-
-//The Promise's .then() mechanism calls our callback function and passes a resolved vale
-//which is in the PromiseResult field (of returned promise object)to the callback function
-//That is how we are able to access data inside a callback function
-
-res.then(function(data) { 
-    console.log(data);
-    const actualRes = JSON.stringify(data.body.data)
-    console.log(actualRes)
+const promise = createOrder(cart);
+console.log(promise);
+promise.then((data) => {
+    console.log(data)
+}).catch((err) => {
+    console.log("I have found an error", err)
 })
 
+//creating our own promise
+function createOrder(cart){
+    const pr = new Promise((resolve, reject) => {
+        if(!validateCart(cart)){
+            const err = new Error("Cart is empty");
+            reject(err);
+        }
+        const orderId = "12345";
+        if(orderId){
+            resolve(orderId)
+        }
+    })
+    return pr;
+}
 
-createOrder(cart).
-    then(function(orderId){
-        return proceedtoPayment(orderId);
-    })
-    .then(function (paymentInfo){
-        return showOrderSummary(paymentInfo)
-    })
-    .then(function (paymentInfo){
-        return updateWalletbalance(paymentInfo);
-    })
+function validateCart(cart){
+    return false;
+}
