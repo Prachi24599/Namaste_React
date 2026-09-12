a50c329238666e3a6241f3a7971a2833bee5dce6
Prachi24599 <prachi24599@gmail.com>
2026-09-13 00:29:38 +0200

    Promise and Promise Chain

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const data = [
-    { firstname : "John", lastname: "Doe", age: 30 },
-    { firstname : "Jane", lastname: "Smith", age: 25 },
-    { firstname : "Bob", lastname: "Johnson", age: 30 },
-    { firstname : "Alice", lastname: "Williams", age: 28 }
-]
+const github_api = "https://api.github.com/users/prachi24599"
 
-//Find the first name of all the people whose age is less than 30
-const ageLessThanThirty = data.filter(person => person.age < 30).map(person => person.firstname)
+const res = fetch(github_api);
+console.log(res);
 
-console.log(ageLessThanThirty)
 
-const ageLessThanThirtyReduce = data.reduce((acc, curr) => {
-    if(curr.age < 30){
-        acc.push(curr.firstname);
-    }
-    return acc;
-}, []) 
-console.log(ageLessThanThirtyReduce)
+//The Promise's .then() mechanism calls our callback function and passes a resolved vale
+//which is in the PromiseResult field (of returned promise object)to the callback function
+//That is how we are able to access data inside a callback function
+
+res.then(function(data) { 
+    console.log(data);
+    const actualRes = JSON.stringify(data.body.data)
+    console.log(actualRes)
+})
+
+
+createOrder(cart).
+    then(function(orderId){
+        return proceedtoPayment(orderId);
+    })
+    .then(function (paymentInfo){
+        return showOrderSummary(paymentInfo)
+    })
+    .then(function (paymentInfo){
+        return updateWalletbalance(paymentInfo);
+    })
