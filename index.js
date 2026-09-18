0c12afcf11a1450ba91fba3f91172afeb012badc
Prachi24599 <prachi24599@gmail.com>
2026-09-18 12:22:46 +0200

    await

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-//wwhat is async - async is a keyword that is used before the function to create a async function
-//We can return promise from it, If we return a value, It will wrap it inside the promise and then return
+//what is await - await is a keyword that can only be used inside a async function
 
+//Example
 const p = new Promise((resolve, reject) => {
-    resolve("The Promise is resolved!");
+    resolve("Promise is resolved!");
 })
-async function getData() {
-    // return "Namaste"
-    return p
+
+async function getData(){
+    const data = await p;
+    console.log(data);
 }
 
-const dataPromise = getData();
-console.log(dataPromise);
-// [[Prototype]]
-// : 
-// Promise
-// [[PromiseState]]
-// : 
-// "fulfilled"
-// [[PromiseResult]]
-// : 
-// "Namaste"
-
-//How to get the actual result from dataPromise i.e. return promise
-//It is the same way we handled promises
-
-dataPromise.then((res) => console.log(res));
-//Namaste
+getData();
