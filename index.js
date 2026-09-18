4f54d15b40d9c00695eae072779e2be365c79c00
Prachi24599 <prachi24599@gmail.com>
2026-09-18 14:10:03 +0200

    multiple promises in single function

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 //what is await - await is a keyword that can only be used inside a async function
 
 //Example
-const p = new Promise((resolve, reject) => {
+const p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-            resolve("Promise is resolved!");
+            resolve("P1 Promise is resolved!");
     }, 10000)
 })
 
-//If we use await keyword, Then JS Enginer waits on the same line until the promise is settled
-// async function getData(){
-//     console.log("Before calling!")
-//     const data = await p;
-//     console.log(data);
-//     console.log("After result")
-// }
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+            resolve("P2 Promise is resolved!");
+    }, 10000);
+})
 
-// getData();
+async function getData() {
+    console.log("Hello");
+    const res1 = await p1;
+    console.log(res1);
 
+    const res2 = await p2;
+    console.log(res2);
+    console.log("Bye");
+}
 
-//In case of promise.then, It wont wait on the same line
-//It will register the promise and continue its execution
-//It will first print namaste js and then after 10 second, the result of promise
-function getResult(){
-    p.then((res) => {console.log(res)});
-    console.log("namaste js")
-}   
-getResult();
+getData();
