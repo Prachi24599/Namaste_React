0efc8e8fde6c56d5fd5b4b6e8f64ed7092568db8
Prachi24599 <prachi24599@gmail.com>
2026-09-18 12:56:17 +0200

    async and await

--- index.js
+++ index.js
@@ -2,12 +2,27 @@
 
 //Example
 const p = new Promise((resolve, reject) => {
-    resolve("Promise is resolved!");
+    setTimeout(() => {
+            resolve("Promise is resolved!");
+    }, 10000)
 })
 
-async function getData(){
-    const data = await p;
-    console.log(data);
-}
+//If we use await keyword, Then JS Enginer waits on the same line until the promise is settled
+// async function getData(){
+//     console.log("Before calling!")
+//     const data = await p;
+//     console.log(data);
+//     console.log("After result")
+// }
 
-getData();
+// getData();
+
+
+//In case of promise.then, It wont wait on the same line
+//It will register the promise and continue its execution
+//It will first print namaste js and then after 10 second, the result of promise
+function getResult(){
+    p.then((res) => {console.log(res)});
+    console.log("namaste js")
+}   
+getResult();
