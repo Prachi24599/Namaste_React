8618db038dee6143da72f4142a60cc2dfcbcdeac
Prachi24599 <prachi24599@gmail.com>
2026-09-21 19:19:26 +0200

    type annotation

--- src/typesInTs.ts
+++ src/typesInTs.ts
@@ -5,4 +5,11 @@ let chai = "masala chai";
 //If we try to assign any other data type to it then it throws an error
 const res = Math.random() > 0.5 ? 10 : 5 //It assumed the result to be number
 
+//Type Annotation - We specify the type (in small letter)
+let world : string = "Hello World";
+let name : string;
 
+name = "Prachi"
+
+const calculate : number = 56;
+// calculate = true; throw an error
