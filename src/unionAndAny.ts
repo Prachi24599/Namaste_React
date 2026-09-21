bd55b4f1ee9c2d6549d167df2650921dd2d22513
Prachi24599 <prachi24599@gmail.com>
2026-09-21 19:35:21 +0200

    union and any

---
+++ src/unionAndAny.ts
@@ -0,0 +1,8 @@
+//Union = By using pipe sign we can specify multiple data types
+//not just on default data types but on our custom data types as well
+let sub : string | number = "1M";
+
+let apiResponseStatus : "pending" | "success" | "error" =  "pending";
+
+//Any - we dont specify any type, better not to use it
+let res;
