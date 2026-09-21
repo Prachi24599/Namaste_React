fae721dd4619f09b6e714de97d71c5978aaffd4d
Prachi24599 <prachi24599@gmail.com>
2026-09-21 17:04:48 +0200

    removed old file

--- index.js
+++
@@ -1,9 +0,0 @@
-const API_URL = "https://api.github.com/users/prachi24599";
-
-async function getData(params) {
-    const res = await fetch(API_URL);
-    console.log(res);
-    const data = await res.json();
-    console.log(data);
-}
-getData()
