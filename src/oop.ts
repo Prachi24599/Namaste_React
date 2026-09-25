04a1260b2893b8b4057b8b6392eb883eba6f18b8
Prachi24599 <prachi24599@gmail.com>
2026-09-25 23:56:43 +0200

    public and private access modifiers

--- src/oop.ts
+++ src/oop.ts
@@ -1,13 +1,26 @@
-class Chai{
-    flavour : string;
-    price : number;
+// class Chai{
+//     flavour : string;
+//     price : number;
 
-    constructor(flavour : string, price : number){
-        this.flavour = flavour;
-        this.price = price;
+//     constructor(flavour : string, price : number){
+//         this.flavour = flavour;
+//         this.price = price;
+//     }
+// }
+// const myTea = new Chai("masala", 20);
+// // myTea.flavour = "aaam"
+// console.log(myTea)
+
+//access modifiers
+class Chai {
+    public flavour : string = "masala";
+    private secreat : string = "cardamom";
+
+    accessPrivate() {
+        return this.secreat;
     }
 }
 
-const myTea = new Chai("masala", 20);
-// myTea.flavour = "aaam"
-console.log(myTea)
+const c = new Chai();
+console.log(c)
+console.log(c.accessPrivate()) //we can access private variable but using a method
